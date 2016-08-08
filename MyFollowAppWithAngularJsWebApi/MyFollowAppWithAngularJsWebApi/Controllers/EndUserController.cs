7f82fd6ea5e92410b8ea83306a450dc455fde52c
prince <prince@promactinfo.com>
2016-08-08 12:10:47 +0530

    updated view ProductOwner Login

---
+++ MyFollowAppWithAngularJsWebApi/MyFollowAppWithAngularJsWebApi/Controllers/EndUserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFollowAppWithAngularJsWebApi.Controllers
+{
+    public class EndUserController : Controller
+    {
+        // GET: EndUser
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFollowAppWithAngularJsWebApi/MyFollowAppWithAngularJsWebApi/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Facebook;
+using Microsoft.Owin.Security.OAuth;
+
+[assembly: OwinStartup(typeof(MyFollowAppWithAngularJsWebApi.Startup))]
+
+namespace MyFollowAppWithAngularJsWebApi
+{
+    public partial class Startup
+    {
+        
+        
+        public void Configuration(IAppBuilder app)
+        {
+            
+            ConfigureAuth(app);
+        }
+    }
+}
