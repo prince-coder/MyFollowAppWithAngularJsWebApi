06fd014f575d0caf44e7389db58a8eb2c117a36a
prince <prince@promactinfo.com>
2016-08-08 14:59:56 +0530

    change in productownerrequest get method

---
+++ MyFollowAppWithAngularJsWebApi/MyFollowAppWithAngularJsWebApi/Models/MyFollow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyFollowAppWithAngularJsWebApi.Models
+{
+    public abstract class MyFollow :AddressDetails
+    {
+        //[Key]
+        //public int UserId { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Please Enter Valid Email Address")]
+        public string Email { get; set; }
+       
+        public string ContactNo { get; set; }
+        //public string TempId { get; set; }
+    }
+}
